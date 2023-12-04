4c49f95cd8e35f2e357431860c9ecea038716c50
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-12-04 23:01:39 +0100

    remove logo from header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,8 +1,7 @@
 import ProfileContainer from "./ProfileContainer";
 
 function Header () {
-    return <div className=" flex items-center justify-between px-8 py-6 fixed inset-x-0 top-0">
-        <div>logo</div>
+    return <div className=" flex items-center justify-end px-8 py-6 fixed inset-x-0 top-0">
         <ProfileContainer />
     </div>
 }
